ae224f0b0e2bcb9cffba28f8f98fc0758c190730
Hieu Tran <33086916+tinasot9874@users.noreply.github.com>
2020-05-07 00:58:34 +0700

    init controller

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+// Define DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "smallblog");
+
+//Define URL
+
+define("ROOT_PATH", "/");
+define("ROOT_URL", "http://localhost/www/PHP/smallBlog-OOP-PHP");
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require_once ("config.php");
+
+require_once ("classes/bootstrap.php");
+require_once ("classes/Controller.php");
+require_once ("classes/Model.php");
+
+require_once ("controllers/home.php");
+require_once ("controllers/shares.php");
+require_once ("controllers/users.php");
+
+require_once ("models/home.php");
+require_once ("models/share.php");
+require_once ("models/user.php");
+
+
+
+$bootstrap      = new Boostrap($_GET);
+$controller     = $bootstrap->createController();
+
+if ($controller){
+    $controller->executeAction();
+}
